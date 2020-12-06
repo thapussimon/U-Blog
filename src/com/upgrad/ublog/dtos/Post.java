8553650ffb6858420e30f9e6a712b18bd9806273
Thapus Simon <71344160+thapussimon@users.noreply.github.com>
2020-12-06 12:12:00 +0530

    CHeckpoint 1

--- src/com/upgrad/ublog/dtos/Post.java
+++ src/com/upgrad/ublog/dtos/Post.java
@@ -1,5 +1,7 @@
 package com.upgrad.ublog.dtos;
 
+import java.time.LocalDateTime;
+
 /**
  * TODO: 1.6. Declare 6 private instance variables in this class named as postId,
  *  emailId, tag, title, description and timestamp. Out of these 6 variables, postId
@@ -14,33 +16,100 @@ package com.upgrad.ublog.dtos;
  */
 
 public class Post {
+    private int postId;
+    private String emailId;
+    private String tag;
+    private String title;
+    private String description;
+    private LocalDateTime timestamp;
 
-//    @Override
-//    public String toString() {
-//        return "Post{" +
-//                "postId=" + postId +
-//                ", emailId='" + emailId + '\'' +
-//                ", tag='" + tag + '\'' +
-//                ", title='" + title + '\'' +
-//                ", description='" + description + '\'' +
-//                ", timestamp=" + timestamp +
-//                '}';
-//    }
+    public Post(){
 
-//    public static void main(String[] args) {
-//        Post post = new Post();
-//        post.setPostId(1);
-//        post.setEmailId("dummy@dummy.com");
-//        post.setTag("tag");
-//        post.setTitle("title");
-//        post.setDescription("Description");
-//        post.setTimestamp(LocalDateTime.now());
-//
-//        System.out.println(post);
-//
-//        /**
-//         * Your output should be similar to as shown below.
-//         * Post{postId=1, emailId='dummy@dummy.com', tag='tag', title='title', description='Description', timestamp=2020-05-22T21:01:34.483363800}
-//         */
-//    }
+    }
+
+    public Post(int postId, String emailId, String tag, String title, String description, LocalDateTime timestamp) {
+        this.postId = postId;
+        this.emailId = emailId;
+        this.tag = tag;
+        this.title = title;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+        @Override
+    public String toString() {
+        return "Post{" +
+                "postId=" + postId +
+                ", emailId='" + emailId + '\'' +
+                ", tag='" + tag + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.setPostId(1);
+        post.setEmailId("dummy@dummy.com");
+        post.setTag("tag");
+        post.setTitle("title");
+        post.setDescription("Description");
+        post.setTimestamp(LocalDateTime.now());
+
+        System.out.println(post);
+
+        /**
+         //         * Your output should be similar to as shown below.
+         //         * Post{postId=1, emailId='dummy@dummy.com', tag='tag', title='title', description='Description', timestamp=2020-05-22T21:01:34.483363800}
+         //         */
+    }
 }
